e7011dbb3920e1569517d96ce19d04bff86f97f5
Daniel Pérez <daniel_p0818@hotmail.com>
2022-10-18 17:04:49 -0500

    list of classes for teachers

--- app/teachers/views.py
+++ app/teachers/views.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from app.database import Database
 from app.config import Access_code
 from app.models import Userdata
+from app.database import db
 
 teachers = Blueprint('teachers',__name__, url_prefix='/teachers',template_folder='templates') 
 
@@ -18,6 +19,20 @@ def home():
 @teachers.route('/classes')
 def teacher_classes():
     
+    classes = db.session.execute("SELECT gp.name,s.name FROM grade_groups as gp JOIN grades_subjects as gs "
+        "ON gp.grade_group_id = gs.grade_group_id JOIN subjects as s ON s.subject_id = gs "
+        ".subject_id WHERE gs.teacher_id = :id",{'id':current_user.id})
+
+    lclass = [x for x in classes]
+
+    return render_template('teachers/classes.html',classes=lclass)
+
+@teachers.route('/classes/<string:group>')
+def class_info(group):
+    
+    students = db.session.execute("SELECT c.name, c.lastname FROM children as c JOIN children_grade_groups a"
+        "s cg ON c.child_id = cg.child_id JOIN grade_groups as gg ON cg.grade_group_id = "
+        "gg.grade_group_id WHERE gg.name = :group ",{'group':group})
 
 
-    return render_template('teachers/classes.html')
+    return render_template('teachers/class_info.html',students = students) 
