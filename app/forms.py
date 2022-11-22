f351cdaf222865f130c5338ff9519f9b55044e5d
Daniel Pérez <daniel_p0818@hotmail.com>
2022-11-21 19:21:20 -0500

    add new class event feature in teachers

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from email.policy import default
-from wtforms import StringField, SubmitField, PasswordField, IntegerField, EmailField , RadioField
+from wtforms import StringField, SubmitField, PasswordField, \
+    IntegerField, EmailField , SelectField, TextAreaField, DateField
 from wtforms.validators import DataRequired, Length, InputRequired, EqualTo, AnyOf
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
@@ -13,10 +14,20 @@ class Users(FlaskForm):
 
 
 class New_user(Users):
-    name=StringField("Name*",validators=[DataRequired(),Length(max=20,message="")])
+    name=StringField("Name*",validators=[DataRequired(),Length(max=20,message="")]) 
     lastname=StringField("Lastname*",validators=[DataRequired(),Length(max=20,message="")]) 
     password2 = PasswordField('confirm password*',validators=[InputRequired(),EqualTo('password', message='Passwords are not equal')])
     add_user = SubmitField("Sign Up")    
 
 class Login(Users):
     log_in = SubmitField("Ingresar")
+
+class Events(FlaskForm):
+    event_type = SelectField("Event type*", choices=[('Assignment'),('Exam'),
+        ('Laboratory'),('Other')])
+    name=StringField("Name*",validators=[DataRequired(),Length(max=50,message="")])
+    description=TextAreaField("Description",validators=[Length(max=50,message="")])
+    submit_date = DateField("Submit date*",validators=[DataRequired()])
+    add_event = SubmitField("Add new")    
+
+    
--- app/teachers/views.py
+++ app/teachers/views.py
@@ -1,9 +1,10 @@
 from os import abort
 from flask import Blueprint ,make_response, render_template,flash,redirect,url_for,request,jsonify
 from flask_login import login_user, login_required, logout_user, current_user
-
+from datetime import date
 from app.database import db, QueriedData
-from datetime import date, datetime
+from app.forms import Events
+
 
 teachers = Blueprint('teachers',__name__, url_prefix='/teachers',template_folder='templates') 
 
@@ -26,7 +27,7 @@ def teacher_classes():
 
     return render_template('teachers/classes.html',classes=QueriedData.return_rows(classes))
 
-@teachers.route('/classes/<int:grade_subject>') 
+@teachers.route('/classes/<int:grade_subject>',methods=['GET','POST']) 
 def class_info(grade_subject):
 
     class_ = db.session.execute("SELECT gs.teacher_id,gg.name, s.name FROM grades_subjects as gs "
@@ -36,7 +37,9 @@ def class_info(grade_subject):
     
     if class_data[0] != current_user.id:
         abort(403)
- 
+    
+    form = Events()
+
     students = db.session.execute("SELECT c.name, c.lastname FROM children as c JOIN children_grade_groups as "
         "cg ON c.child_id = cg.child_id JOIN grade_groups as gg ON cg.grade_group_id = gg.grade_group_id "
         "JOIN grades_subjects as gs ON gg.grade_group_id = gs.grade_group_id  WHERE gs.grade_subject_id = :id",{'id':grade_subject})
@@ -60,11 +63,29 @@ def class_info(grade_subject):
     if(len(week_events) < 1):
         no_week_events = True
 
+    if form.add_event.data and form.validate():
+        print(form.event_type.data.lower())
+        
+        try:
+            db.session.execute("INSERT INTO class_events (event_type,name,description,date,bimester,grade_subject_id) "
+                "VALUES (:eve,:nam,:des,:dat,:bim,:id)",{'eve':form.event_type.data.lower(),'nam':form.name.data,
+                'des':form.description.data,'dat':form.submit_date.data,'bim':4,'id':grade_subject})
+            db.session.commit()
+            flash("New event created",category='success')
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+            flash('error',category='Danger') 
+        finally:
+            return redirect (url_for('teachers.class_info',grade_subject = grade_subject))
+
     return render_template('teachers/class_info.html',students = students, group= class_data[1], classs= class_data[2],
-        events = events, week_events = week_events, exams = exams, labs = labs, nwe = no_week_events, grade_subject = grade_subject) 
+        events = events, week_events = week_events, exams = exams, labs = labs, nwe = no_week_events, grade_subject = grade_subject,
+        form = form) 
 
 @teachers.route('/classes/<int:grade_subject>/grades')
 def grades(grade_subject):
+
 
     class_ = db.session.execute("SELECT gs.teacher_id,gg.name, s.name FROM grades_subjects as gs "
         "JOIN grade_groups as gg ON gs.grade_group_id = gg.grade_group_id JOIN subjects as "
@@ -74,12 +95,14 @@ def grades(grade_subject):
     if class_data[0] != current_user.id:
         abort(403)
 
-    students = db.session.execute("SELECT c.child_id,c.name, c.lastname FROM children as c JOIN children_grade_groups as "
+    
+
+    students = db.session.execute("SELECT c.child_id, c.lastname,c.name FROM children as c JOIN children_grade_groups as "
         "cg ON c.child_id = cg.child_id JOIN grade_groups as gg ON cg.grade_group_id = gg.grade_group_id "
         "JOIN grades_subjects as gs ON gg.grade_group_id = gs.grade_group_id  WHERE gs.grade_subject_id = :id "
         "ORDER BY lastname",{'id':grade_subject}) 
     
-    students = QueriedData.return_dic(students,'id','name','lastname') 
+    students = QueriedData.return_dic(students,'id','lastname','name') 
 
     grades = db.session.execute("SELECT e.name, g.child_id, g.grade FROM grades as g JOIN class_events as e ON g.event_id = "
         "e.event_id WHERE grade_subject_id = :id",{'id':grade_subject})
@@ -88,10 +111,11 @@ def grades(grade_subject):
     for x in range (len(grades)):
         for y in students:
             if y['id'] == grades[x][1]:
-                y[grades[x][0]] = grades[x][2] 
-                continue
+                y[grades[x][0]] = grades[x][2]  
+                break
 
     print(students)
 
+    
 
-    return render_template('teachers/grades.html',grades = students)
+    return render_template('teachers/grades.html',grades = students, group = class_data[1], classs = class_data[2])
